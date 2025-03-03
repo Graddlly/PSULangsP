b5639ce546f9ecbc03654735bdcde340749f3747
Graddlly <graddlly@enodestteam.ru>
2025-03-03 14:35:46 +0500

    [FEATURE] F#: Лабораторная работа №2

---
+++ F#/LW2/Test1/Test1/Program.fs
@@ -0,0 +1,42 @@
+﻿open System
+
+// Функция для ввода одного символа
+let rec getPrefix () =
+    printf "Введите один символ, который нужно добавить в начало строк: "
+    match Console.ReadLine() with
+    | null | "" ->
+        printfn "Ошибка: ввод не должен быть пустым. Попробуйте снова.\n"
+        getPrefix ()
+    | input when input.Length > 1 ->
+        printfn "Ошибка: введите только один символ. Попробуйте снова.\n"
+        getPrefix ()
+    | input -> input.[0]
+
+// Функция для ввода списка строк
+let rec getStringList () =
+    printf "Введите строки через запятую: "
+    let inputStrings = Console.ReadLine()
+    match inputStrings with
+    | null | "" ->
+        printfn "Ошибка: ввод не должен быть пустым. Попробуйте снова.\n"
+        getStringList ()
+    | _ ->
+        let stringList = inputStrings.Split(',') |> Array.toList |> List.map (_.Trim()) // (fun s -> s.Trim())
+        stringList
+
+// Функция обработки списка строк
+let addPrefixToList (prefix: char) (strings: string list) =
+    strings |> List.map (sprintf "%c%s" prefix) // (fun str -> sprintf "%c%s" prefix str)
+
+[<EntryPoint>]
+let main _ =
+    printfn "Эта программа добавляет указанный символ в начало каждой строки."
+
+    let prefix = getPrefix()
+    let stringList = getStringList()
+    let modifiedList = addPrefixToList prefix stringList
+
+    printfn "\nРезультат:"
+    modifiedList |> List.iter (printfn "%s")
+
+    0
---
+++ F#/LW2/Test2/Test2/Program.fs
@@ -0,0 +1,35 @@
+﻿open System
+
+// Функция для подсчёта количества строк заданной длины
+let countStringsOfLength length strings =
+    strings |> List.fold (fun acc str -> if String.length str = length then acc + 1 else acc) 0
+
+// Функция для запроса целочисленного ввода у пользователя
+let rec getIntInput prompt =
+    printf "%s" prompt
+    match Int32.TryParse(Console.ReadLine()) with
+    | (true, value) -> value
+    | _ ->
+        printfn "Ошибка ввода! Введите целое число."
+        getIntInput prompt
+
+// Функция для ввода списка строк
+let rec getStringList () =
+    printfn "Введите строки по одной. Для завершения ввода оставьте строку пустой и нажмите Enter."
+    let rec loop acc =
+        let input = Console.ReadLine()
+        match input with
+        | "" -> List.rev acc
+        | _ -> loop (input :: acc)
+    loop []
+
+[<EntryPoint>]
+let main _ =
+    printfn "Программа для подсчёта строк заданной длины."
+    
+    let strings = getStringList()
+    let length = getIntInput "Введите длину строк для подсчёта: "
+    let count = countStringsOfLength length strings
+    
+    printfn "Количество строк длины %d: %d" length count
+    0
