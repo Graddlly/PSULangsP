d97731f1950dc5b2c1dd6b6debc021399f57780e
Graddlly <graddlly@enodestteam.ru>
2025-05-26 13:46:12 +0500

    [STYLE] C#: УКП: IO модуль - исправление авто-свойств

--- С#/PascalCompiler/PascalCompiler.IOModule/Models/Err.cs
+++ С#/PascalCompiler/PascalCompiler.IOModule/Models/Err.cs
@@ -2,12 +2,12 @@ namespace PascalCompiler.IOModule.Models;
 
 public struct Err
 {
+    public TextPosition errorPosition { get; set; }
+    public byte errorCode { get; set; }
+    
     public Err(TextPosition errorPosition, byte errorCode)
     {
         this.errorPosition = errorPosition;
         this.errorCode = errorCode;
     }
-
-    public TextPosition errorPosition { get; set; }
-    public byte errorCode { get; set; }
 }
--- С#/PascalCompiler/PascalCompiler.IOModule/Models/TextPosition.cs
+++ С#/PascalCompiler/PascalCompiler.IOModule/Models/TextPosition.cs
@@ -2,14 +2,14 @@ namespace PascalCompiler.IOModule.Models;
 
 public struct TextPosition
 {
+    public uint lineNumber { get; set; }
+    public byte charNumber { get; set; }
+    
     public TextPosition(uint ln = 0, byte c = 0)
     {
         lineNumber = ln;
         charNumber = c;
     }
-
-    public uint lineNumber { get; set; }
-    public byte charNumber { get; set; }
 
     public override string ToString()
     {
